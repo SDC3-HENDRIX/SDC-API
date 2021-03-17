5dbccd8d1ce303ad00fbad3133342487e90dd26e
Zach Kodjo <36774583+kodjoz@users.noreply.github.com>
2021-03-17 15:05:28 -0400

    Exclude csv files from git, adds my product schema and schema designer file to the project

--- products/products.sql
+++ products/products.sql
@@ -1,47 +1,54 @@
-DROP DATABASE IF EXISTS sd-capstone; /* I may remove this I didn't come here to be judged*/
+DROP DATABASE IF EXISTS products; /* I may remove this I didn't come here to be judged*/
 
-CREATE DATABASE IF NOT EXISTS sd-capstone;
+CREATE DATABASE IF NOT EXISTS products;
 
-USE sd-capstone;
+USE products;
 
 CREATE TABLE IF NOT EXISTS products (
-  product_id INT DEFAULT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(128) DEFAULT NOT NULL,
-  slogan VARCHAR(128) DEFAULT NOT NULL,
-  description TEXT DEFAULT NOT NULL,
-  category VARCHAR(64) DEFAULT NOT NULL,
-  default_price INT DEFAULT NOT NULL,
+  product_id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(128),
+  slogan VARCHAR(128),
+  description TEXT,
+  category VARCHAR(64),
+  default_price INT
 );
 
 CREATE TABLE IF NOT EXISTS features (
-  feature_id INT DEFAULT AUTO_INCREMENT PRIMARY KEY,
-  feature VARCHAR(64) DEFAULT,
-  value VARCHAR(64) DEFAULT
-);
-
-CREATE TABLE IF NOT EXISTS styles (
-  style_id INT DEFAULT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(128) DEFAULT NOT NULL,
-  original_price INT DEFAULT NOT NULL,
-  sale_price INT DEFAULT NOT NULL,
-  'default?' BOOLEAN,
+  feature_id INT AUTO_INCREMENT PRIMARY KEY,
+  feature VARCHAR(64),
+  value VARCHAR(64),
+  product_id INT,
   FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
 
-CREATE TABLE IF NOT EXISTS skus (
-  sku_id INT DEFAULT AUTO_INCREMENT PRIMARY KEY,
-  quantity INT DEFAULT NOT NULL,
+CREATE TABLE IF NOT EXISTS styles (
+  style_id INT  AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(128),
+  original_price INT,
+  sale_price INT,
+  default_style BOOLEAN,
+  product_id INT,
+  FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
+
+/* CREATE TABLE IF NOT EXISTS skus (
+  sku_id INT  AUTO_INCREMENT PRIMARY KEY,
+  quantity INT  NOT NULL,
   size VARCHAR(5),
   FOREIGN KEY (style_id) REFERENCES styles(style_id)
-);
+); */
 
 CREATE TABLE IF NOT EXISTS photos (
-  photo_id INT DEFAULT AUTO_INCREMENT PRIMARY KEY,
-  thumbnail_url VARCHAR(255) DEFAULT,
-  'url' VARCHAR(255) DEFAULT,
+  photo_id INT  AUTO_INCREMENT PRIMARY KEY,
+  thumbnail_url VARCHAR(255),
+  url VARCHAR(255),
+  style_id INT,
   FOREIGN KEY (style_id) REFERENCES styles(style_id)
 );
 
-CREATE TABLE IF NOT EXISTS features (
-
+CREATE TABLE IF NOT EXISTS related (
+  related_id INT AUTO_INCREMENT PRIMARY KEY,
+  related_product_id INT,
+  current_product_id INT,
+  FOREIGN KEY (current_product_id) REFERENCES products(product_id)
 );
