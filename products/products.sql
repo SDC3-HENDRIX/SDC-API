f201dafbe76615d0129095c38c9428d92d7acc38
Zach Kodjo <36774583+kodjoz@users.noreply.github.com>
2021-03-18 10:39:13 -0400

    Revised products sql schema after uploading data to MariaDB

--- products/products.sql
+++ products/products.sql
@@ -4,10 +4,13 @@ CREATE DATABASE IF NOT EXISTS products;
 
 USE products;
 
+GRANT ALL ON products.* to student@localhost;
+FLUSH PRIVILEGES;
+
 CREATE TABLE IF NOT EXISTS products (
   product_id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(128),
-  slogan VARCHAR(128),
+  slogan TEXT,
   description TEXT,
   category VARCHAR(64),
   default_price INT
@@ -31,17 +34,18 @@ CREATE TABLE IF NOT EXISTS styles (
   FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
 
-/* CREATE TABLE IF NOT EXISTS skus (
-  sku_id INT  AUTO_INCREMENT PRIMARY KEY,
-  quantity INT  NOT NULL,
-  size VARCHAR(5),
+CREATE TABLE IF NOT EXISTS skus (
+  sku_id INT AUTO_INCREMENT PRIMARY KEY,
+  quantity INT,
+  size VARCHAR(16),
+  style_id INT,
   FOREIGN KEY (style_id) REFERENCES styles(style_id)
-); */
+);
 
 CREATE TABLE IF NOT EXISTS photos (
   photo_id INT  AUTO_INCREMENT PRIMARY KEY,
-  thumbnail_url VARCHAR(255),
-  url VARCHAR(255),
+  thumbnail_url TEXT,
+  url TEXT,
   style_id INT,
   FOREIGN KEY (style_id) REFERENCES styles(style_id)
 );
