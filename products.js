8552b568c1b4d5e3750246d0b852a253f4f15a06
Zach Kodjo <36774583+kodjoz@users.noreply.github.com>
2021-03-23 11:20:13 -0400

    bug fixes for products

--- products.js
+++ products.js
@@ -4,88 +4,90 @@ const logger = require('./config/winston');
 const { redisSet, redisGet } = require('./config/redis');
 
 const router = express.Router();
-
 // destination for products node instance
 const destination = 'http://localhost:3010';
 
-router.get('/products', (req, res) => {
+router.get('/', (req, res) => {
   const page = req.query.page || 1;
   const count = req.query.count || 5;
   const key = `page:${page}:count:${count}`;
   // this is a promise
   return redisGet(key).then((result) => {
     if (result) {
-      res.status(200).send(JSON.parse(result));
-    } else {
-      return axios.get(`${destination}/products?page=${page}&count=${count}`)
-        .then(({ data }) => {
-          res.send(data);
-          return redisSet(key, JSON.stringify(data));
-        })
-        .catch((err) => logger.error(err));
+      return res.send(JSON.parse(result));
     }
+
+    return axios.get(`${destination}/products?page=${page}&count=${count}`)
+      .then(({ data }) => {
+        res.send(data);
+        return redisSet(key, JSON.stringify(data));
+      })
+      .catch((err) => {
+        logger.error(err);
+        res.status(500).send('There was an error getting products');
+      });
   });
 });
 
 // get one product from the endpoint
-router.get('/products/:product_id', (req, res) => {
+router.get('/:product_id', (req, res) => {
   const productId = req.params.product_id;
   const key = `product:${productId}`;
+  return redisGet(key).then((result) => {
+    if (result !== null) {
+      return res.send(JSON.parse(result));
+    }
 
-  if (productId) {
-    return redisGet(key).then((result) => {
-      if (result !== null) {
-        res.status(200).send(JSON.parse(result));
-      } else {
-        return axios.get(`${destination}/products/${productId}`)
-          .then(({ data }) => {
-            res.status(200).send(data);
-            return redisSet(key, JSON.stringify(data));
-          })
-          .catch((err) => logger.error(err));
-      }
-    });
-  }
+    return axios.get(`${destination}/products/${productId}`)
+      .then(({ data }) => {
+        res.status(200).send(data);
+        return redisSet(key, JSON.stringify(data));
+      })
+      .catch((err) => {
+        res.status(500).send(`There was an error getting ${productId}`);
+        logger.error(err);
+      });
+  });
 });
 
-router.get('/products/:product_id/styles', (req, res) => {
+router.get('/:product_id/styles', (req, res) => {
   const productId = req.params.product_id;
   const key = `product:${productId}:styles`;
 
-  if (productId) {
-    return redisGet(key).then((result) => {
-      if (result !== null) {
-        res.status(200).send(JSON.parse(result));
-      } else {
-        return axios.get(`${destination}/products/${productId}/styles`)
-          .then(({ data }) => {
-            res.status(200).send(data);
-            return redisSet(key, JSON.stringify(data));
-          })
-          .catch((err) => logger.error(err));
-      }
-    });
-  }
+  return redisGet(key).then((result) => {
+    if (result !== null) {
+      return res.send(JSON.parse(result));
+    }
+    return axios.get(`${destination}/products/${productId}/styles`)
+      .then(({ data }) => {
+        res.status(200).send(data);
+        return redisSet(key, JSON.stringify(data));
+      })
+      .catch((err) => {
+        res.status(500).send(`There was an error getting styles for ${productId}`);
+        logger.error(err);
+      });
+  });
 });
 
-router.get('/products/:product_id/related', (req, res) => {
+router.get('/:product_id/related', (req, res) => {
   const productId = req.params.product_id;
   const key = `product:${productId}:related`;
 
-  if (productId) {
-    return redisGet(key).then((result) => {
-      if (result !== null) {
-        res.status(200).send(JSON.parse(result));
-      } else {
-        return axios.get(`${destination}/products/${productId}/related`)
-          .then(({ data }) => {
-            res.status(200).send(data);
-            return redisSet(key, JSON.stringify(data));
-          })
-          .catch((err) => logger.error(err));
-      }
-    });
-  }
+  return redisGet(key).then((result) => {
+    if (result !== null) {
+      return res.send(JSON.parse(result));
+    }
+    return axios.get(`${destination}/products/${productId}/related`)
+      .then(({ data }) => {
+        res.status(200).send(data);
+        return redisSet(key, JSON.stringify(data));
+      })
+      .catch((err) => {
+        res.status(500).send(`There was an error getting related products for ${productId}`);
+        logger.error(err);
+      });
+  });
 });
 
 module.exports = router;
