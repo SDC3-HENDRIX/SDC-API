75264fd2c5a3ec6dbbc58148d7dac51e209556e3
Zach Kodjo <36774583+kodjoz@users.noreply.github.com>
2021-03-23 15:28:50 -0400

    Changes to bootstrap script

--- setup.bash
+++ setup.bash
@@ -3,15 +3,14 @@
 sudo apt update -y
 sudo apt dist-upgrade -y
 sudo apt autoremove -y
-sudo apt install -y curl flex bison gcc g++ checkinstall tcl8.6 tcl8.6-dev
+sudo apt install -y curl wget flex bison gcc g++ checkinstall tcl8.6 tcl8.6-dev pkg-config ruby2.7 ruby-full
 
 wget https://download.redis.io/releases/redis-6.2.1.tar.gz
+wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 exec bash
 nvm install --lts
 nvm use --lts
-git clone https://github.com/SDC3-HENDRIX/SDC-API.git
-
-tar -xzvf redis-6.2.1.tar.gz
-
+chmod +x ./install
+sudo ./install auto > /tmp/logfile
 
