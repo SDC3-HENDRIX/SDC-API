5f3ee9bc2cc24f7677c8cff0cf5c26af04736451
Zach Kodjo <36774583+kodjoz@users.noreply.github.com>
2021-03-25 12:23:49 -0400

    Allow clustering

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 const express = require('express');
 const morgan = require('morgan');
+const cluster = require('cluster');
+const compression = require('compression');
+const numCPUs = require('os').cpus().length;
 const products = require('./products');
 const questions = require('./questions');
 const reviews = require('./reviews');
 const logger = require('./config/winston');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(morgan('combined', { stream: logger.stream }));
+app.use(compression());
 
 // proxy routes
 app.use('/products', products);
-app.use('/qa', questions);
-app.use('/reviews', reviews);
+// app.use('/qa', questions);
+// app.use('/reviews', reviews);
 
-app.listen(port);
+if (cluster.isMaster) {
+  for (let i = 0; i < numCPUs; i += 1) {
+    cluster.fork();
+  }
+  cluster.on('exit', (worker) => {
+    logger.info(`worker ${worker.process.pid} died`);
+    cluster.fork();
+  });
+} else {
+  app.listen(port);
+}
 
 // API key for loader.io
 app.get('/loaderio-cc6341b7f5678d3e2579c9a1e733a07e', (req, res) => {
