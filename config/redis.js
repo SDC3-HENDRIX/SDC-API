7a1e897408bad753bb833e4203732bba4ee49a82
Zach Kodjo <zach.n.kodjo@gmail.com>
2021-03-26 16:39:25 -0400

    Products: Changes to API for docker (#9)
    * Added http-proxy-middleware and removed products.sql
    
    * Eslint fixes
    
    * Switched from Airbnb to Airbnb-base
    
    * Added winston logger
    
    * Moved products proxy to new file
    
    * New file for products caching
    
    * Moved winston to config dir
    
    * Moved winston
    
    * Refactored app to use router middleware for products
    
    * Eslint fixes
    
    * Installed redis
    
    * Created route handling for caching proxy
    
    * Redis config
    
    * Remove cruft
    
    * Remove cruft
    
    * rename service in winston
    
    * changed loader.io key
    
    * Created route for questions
    
    * Created bootstrap setup script
    
    * Created questions caching routes
    
    * bug fixes for the actual route
    
    * bug fixes for products
    
    * New routes for reviews
    
    * Bug fixes for questions
    
    * ESLint fixes
    
    * Updated gitignore to ignore database files
    
    * Added comments
    
    * Changes to bootstrap script
    
    * Deleted old setup script
    
    * Created appspec.yml
    
    * Created setup scripts
    
    * Changed appspec based on testing my personal repo, adds redis setup script, sets file permissions
    
    * More changes to fix the file permissions stuff
    
    * Changes to scripts
    
    * Added production endpoint
    
    * Changed destination for products endpoint for dev and prod
    
    * converts project to Docker
    
    * Revised ignore files
    
    * Revised ignore files to exclude docker-compose
    
    * revised Dockerfile
    
    * Allow clustering
    
    * revised redis cleint to accept env vars
    
    * Added npm scripts
    
    * Revised routes to accept env vars
    
    * Added remaining docker dependencies
    
    * Adds redis config
    
    * Creates example config files
    
    * Moved redis and related files to one folder
    
    * Example environment variables
    
    * Edited gitignore for error
    
    * Ignore tar files
    
    * Set expiry for keys
    
    * Revised setup for docker
    
    Co-authored-by: BillMehlenbacher <67293878+BillMehlenbacher@users.noreply.github.com>

--- config/redis.js
+++ config/redis.js
@@ -1,16 +1,15 @@
 const { promisify } = require('util');
 const redis = require('redis');
 
-
 const host = process.env.REDIS_HOST;
 const password = process.env.REDIS_PASS;
 
 const client = redis.createClient({
   host,
   password,
-
 });
 
 // export these as promises
 exports.redisSet = promisify(client.set).bind(client);
+exports.redisSetEx = promisify(client.setex).bind(client);
 exports.redisGet = promisify(client.get).bind(client);
